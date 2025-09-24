34e3f4357ecc59365f3bd165057d9604fc75e708
Atul Prakash <berealatul@gmail.com>
2025-09-25 00:15:29 +0530

    Revert "Merge branch 'main' of https://github.com/berealatul/nba"
    This reverts commit df4636a1695200c2f0585f5c47bff7e26fd8548a, reversing
    changes made to 3001e8bd578c135aa83c311b16eac5df0772105e.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { LoginForm } from "@/components/login-form"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <LoginForm />
-    </div>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
